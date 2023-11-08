e7a173bf207abad798c774707ca134454ac25181
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-11-08 12:59:37 -0500

    bg

--- main.ts
+++ main.ts
@@ -2,7 +2,31 @@
  *
  * Created by: Angelo Yalung
  * Created on: Nov 2023
- * This program ...
+ * This program makes a servo turn to 180 degrees on pressing the B button and 0 degrees on pressing the A button
 */
 
-basic.showString('Hello, World!')
+// variables
+const servo1 = robotbit.Servos.S1
+
+// setup
+basic.clearScreen()
+basic.pause(500)
+basic.showIcon(IconNames.Happy)
+
+// on A Button
+input.onButtonPressed(Button.A, function () {
+  robotbit.Servo(servo1, 0)
+  basic.showNumber(0)
+  basic.showIcon(IconNames.SmallSquare)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
+
+// on B Button
+input.onButtonPressed(Button.B, function () {
+  robotbit.Servo(servo1, 180)
+  basic.showNumber(180)
+  basic.showIcon(IconNames.SmallSquare)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
